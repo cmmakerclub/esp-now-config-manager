18efae8ebae74de6a83f2c9d9c5a93f94d53f454
Nat <nat@cmmc.io>
2017-10-17 14:02:56 +0700

    use led and boot mode library

--- espnow-controller/espnow-controller.ino
+++ espnow-controller/espnow-controller.ino
@@ -3,6 +3,9 @@
 #include <CMMC_SimplePair.h>
 #include <CMMC_Utils.h>
 #include <CMMC_ESPNow.h>
+#include <CMMC_LED.h>
+#include <CMMC_BootMode.h>
+
 extern "C" {
 #include <espnow.h>
 #include <user_interface.h>
@@ -11,9 +14,13 @@ extern "C" {
 #define LED LED_BUILTIN
 #define BUTTON_PIN 13
 
+int mode;
+
 CMMC_SimplePair instance;
-CMMC_ESPNow ESPNow;
+CMMC_ESPNow espNow;
 CMMC_Utils utils;
+CMMC_LED led(LED, HIGH);
+CMMC_BootMode bootMode(&mode, BUTTON_PIN);
 
 bool ledState = LOW;
 
@@ -52,23 +59,40 @@ void check_boot_mode() {
     WiFi.mode(WIFI_STA);
     delay(50);
     Serial.print("Initializing... Controller..");
-    ESPNow.init(NOW_MODE_CONTROLLER);
-    ESPNow.on_message([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
-      // PACKET_T pkt;
-      // memcpy(&pkt, data, sizeof(pkt));
-      // memcpy(&pkt.from, macaddr, 48);
-      // SENSOR_T sensorData = pkt.data;
-      Serial.write(data, len);
-      digitalWrite(LED, ledState);
-      ledState = !ledState;
-    });
+    espNow.init(NOW_MODE_CONTROLLER);
+//    espNow.on_message([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
+//      // PACKET_T pkt;
+//      // memcpy(&pkt, data, sizeof(pkt));
+//      // memcpy(&pkt.from, macaddr, 48);
+//      // SENSOR_T sensorData = pkt.data;
+//      Serial.write(data, len);
+//      led.toggle();
+//    });
   }
+}
+
+void start_config_mode() {
+  instance.begin(MASTER_MODE, evt_callback);
+  instance.debug([](const char* s) {
+    Serial.printf("[USER]: %s\r\n", s);
+  });
+  instance.start();
 }
 
 void setup()
 {
   setup_hardware();
-  check_boot_mode();
+  bootMode.check([](int mode) {
+    if (mode == BootMode::MODE_CONFIG) {
+      start_config_mode();
+    }
+    else if (mode == BootMode::MODE_RUN) {
+      espNow.init(NOW_MODE_SLAVE);
+    }
+    else {
+      // unhandled
+    }
+  });
 }
 
 void loop()
