d9a81ed7c7760696baba25398025d0e82a6f2dc8
Nat <nat@cmmc.io>
2017-12-08 08:23:14 +0700

    clear changes.

--- espnow-slave-bme280/espnow-slave-bme280/espnow-slave-bme280.ino
+++ espnow-slave-bme280/espnow-slave-bme280/espnow-slave-bme280.ino
@@ -71,10 +71,10 @@ void setup()
   Serial.println("starting...");
   led.init();
   Wire.begin();
-  bme.begin(0x76);
+  bme.begin(0x77);
   configManager.init("/config98.json");
   pinMode(PROD_MODE_PIN, INPUT_PULLUP); 
-  uint32_t wait_config = 50;
+  uint32_t wait_config = 1000;
   if (digitalRead(PROD_MODE_PIN) == LOW) {
     wait_config = 0; 
   } 
@@ -99,39 +99,39 @@ void setup()
 CMMC_SENSOR_T packet;
 
 void read_sensor() {
-  packet.type = 99;
+  packet.type = 8;
   packet.battery = analogRead(A0);
   memcpy(packet.to, master_mac, 6);
   memcpy(packet.from, self_mac, 6); 
-  strcpy(packet.myName, "DUMMY-ID-01");
+  strcpy(packet.myName, "LATTE-ID-01");
   packet.nameLen = strlen(packet.myName); 
 
-  // bool read_ok = 0;
-  // while(!read_ok) {
-  //   float t = bme.readTemperature();
-  //   float h = bme.readHumidity();
-  //   Serial.println(h);
+  bool read_ok = 0;
+  while(!read_ok) {
+    float t = bme.readTemperature();
+    float h = bme.readHumidity();
+    Serial.println(h);
 
-  //   if (isnan(h) || h == 0) {
-  //     Serial.println("read bme280 failed... try again..");
-  //     delay(1000); 
-  //   }
-  //   else {
-  //     packet.field1 = t * 100;
-  //     packet.field2 = h * 100;
-  //     packet.field3 = bme.readPressure(); 
-  //     break;
-  //   }
-  // }
-  //   packet.ms = millis();
+    if (isnan(h) || h == 0) {
+      Serial.println("read bme280 failed... try again..");
+      delay(1000); 
+    }
+    else {
+      packet.field1 = t * 100;
+      packet.field2 = h * 100;
+      packet.field3 = bme.readPressure(); 
+      break;
+    }
+  }
+    packet.ms = millis();
     packet.sent_ms = millis();
     packet.sum = CMMC::checksum((uint8_t*) &packet,
                               sizeof(packet) - sizeof(packet.sum));
 
-  //  Serial.printf("battery     %lu(%02x)\r\n", packet.battery, packet.battery);
-  //  Serial.printf("temperature %lu(%02x)\r\n", packet.field1, packet.field1);
-  //  Serial.printf("humidity    %lu(%02x)\r\n", packet.field2, packet.field2);
-  //  Serial.printf("pressure    %lu(%02x)\r\n", packet.field3, packet.field3);
+   Serial.printf("battery     %lu(%02x)\r\n", packet.battery, packet.battery);
+   Serial.printf("temperature %lu(%02x)\r\n", packet.field1, packet.field1);
+   Serial.printf("humidity    %lu(%02x)\r\n", packet.field2, packet.field2);
+   Serial.printf("pressure    %lu(%02x)\r\n", packet.field3, packet.field3);
 }
 
 void loop()
@@ -139,16 +139,16 @@ void loop()
   Serial.printf("millis() = %lu\r\n", millis());
   read_sensor(); 
   packet.sent_ms = millis();
-  // packet.sent_ms = millis();
-  // if (master_mac[0] == 0x00 && master_mac[1] == 0x00) {
-  //   goSleep(DEFAULT_DEEP_SLEEP_S);
-  // }
-  // else {
+  packet.sent_ms = millis();
+  if (master_mac[0] == 0x00 && master_mac[1] == 0x00) {
+    goSleep(DEFAULT_DEEP_SLEEP_S);
+  }
+  else {
     espNow.enable_retries(true);
     Serial.println(millis());
     espNow.send(master_mac, (u8*)&packet, sizeof (packet), []() { }, 1000);
-  // } 
-  // delay(10); 
+  } 
+  delay(10); 
 }
 
 void goSleep(uint32_t deepSleepS) {
